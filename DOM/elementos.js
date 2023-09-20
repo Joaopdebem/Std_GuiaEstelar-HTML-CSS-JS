550e4f587d8f6952fc62fca82051a8ca62b4dc7f
João Pedro de Bem <jpdebem@gmail.com>
2023-09-20 09:34:09 -0300

    Commit inicial

---
+++ DOM/elementos.js
@@ -0,0 +1,47 @@
+// Navegando pelos elementos
+
+// parentNode parentElement
+//const body = document.querySelector('body')
+//console.log(body.parentElement)
+
+// childNodes children
+//const el = document.querySelector('body')
+//console.log(el.childNodes)
+
+// firstChild firstElementChild
+//const el = document.querySelector('body')
+//console.log(el.firstElementChild)
+
+// lastChild lastElementChild
+//const el = document.querySelector('body')
+//console.log(el.lastElementChild)
+
+// nextSibling nextElementSibling
+//const el = document.querySelector('script')
+//console.log(el.nextElementSibling)
+// previousSibling previousElementSibling
+
+// createElement 'Criei a DIV'
+//const div = document.createElement('div')
+//div.innerText = "Olá Devs!"
+//const text = document.createElement('p')
+//text.innerText = "Texto aqui"
+
+// append 'pós' prepend 'antes'
+//const body = document.querySelector('body')
+//body.append(div)
+//body.prepend(text)
+
+// Adicionando elementos
+//const div = document.createElement('div');
+//div.innerText = "Salve salve familia"
+//const div2 = document.createElement('div');
+//div2.innerText = "Boa tarde comunidade"
+
+
+// insertBefore
+//const body = document.querySelector('body')
+//const header = body.querySelector('header')
+//const script = body.querySelector('script')
+//body.insertBefore(div, header.nextElementSibling)
+//body.insertBefore(div2, header)
---
+++ DOM/getElement.js
@@ -0,0 +1,20 @@
+// getElementByID() 'element'
+//const element = document.getElementById('blog-title')
+//console.log(element)
+
+
+// getElementByClassName() 'HTMLColection'
+//const element = document.getElementsByClassName("one");
+//console.log(element)
+
+
+// getElementByTagName() 'HTMLColection'
+//const element = document.getElementsByTagName("body")
+//console.log(element)
+
+// querySelect() 'element'
+
+// querySelectAll() 'Nodelist'
+//const elements = document.querySelectorAll(".one");
+//console.log(elements)
+//elements.forEach(el => console.log(el))
